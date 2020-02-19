3807a853522a02bce3e8c60f8ef2a7bf4e81558e
kgm78 <staticel78@gmail.com>
2020-02-19 18:57:24 -0500

    Made skeleton of CubiChat class

--- src/CubiChat.java
+++ src/CubiChat.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-public class CubiChat {
+public class CubiChat extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        GridPane pane = new GridPane();
+        Label chatLbl = new Label("Chat");
+        TextField inputTxt = new TextField();
+        Button btnSend = new Button("Send");
+        pane.add(chatLbl, 0, 0);
+        pane.add(inputTxt, 1, 0);
+        pane.add(btnSend, 1, 1);
+        stage.setScene(new Scene(pane, 500, 500));
+        stage.setTitle("CubiChat");
+        stage.show();
+    }
 }
