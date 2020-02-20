a0d0cca4075a77503f246617757e0b5fbbed83d7
kgm78 <staticel78@gmail.com>
2020-02-19 19:10:43 -0500

    Added comments

--- src/CubiChat.java
+++ src/CubiChat.java
@@ -10,13 +10,17 @@ public class CubiChat extends Application {
 
     @Override
     public void start(Stage stage) {
+        //Creates gridpane named pane
         GridPane pane = new GridPane();
+        //Creates elements
         Label chatLbl = new Label("Chat");
         TextField inputTxt = new TextField();
         Button btnSend = new Button("Send");
+        //Adds elements to pane
         pane.add(chatLbl, 0, 0);
         pane.add(inputTxt, 1, 0);
         pane.add(btnSend, 1, 1);
+        //Creates stage and adds a scene with pane and launches GUI
         stage.setScene(new Scene(pane, 500, 500));
         stage.setTitle("CubiChat");
         stage.show();
